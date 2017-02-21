25fde1bf197a7b6a6dd165bc989ad41e9179446b
William Alexis Ortiz <waortiz@hotmail.com>
2017-02-21 09:28:21 -0500

    Adding missing files.

---
+++ SuperShoes.Api/Filters/IdentityBasicAuthenticationAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace SuperShoes.Api.Filters
+{
+    using System.Security.Claims;
+    using System.Security.Principal;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class IdentityBasicAuthenticationAttribute : BasicAuthenticationAttribute
+    {
+        protected override async Task<IPrincipal> AuthenticateAsync(string userName, string password, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested(); // Unfortunately, UserManager doesn't support CancellationTokens.
+            if (!(userName.Equals("my_user", System.StringComparison.InvariantCultureIgnoreCase) && password == "my_password"))
+            {
+                return null;
+            }
+
+            // Create a ClaimsIdentity with all the claims for this user.
+            cancellationToken.ThrowIfCancellationRequested();
+            ClaimsIdentity identity = new ClaimsIdentity(new GenericIdentity(userName));
+            return new ClaimsPrincipal(identity);
+        }
+    }
+}
